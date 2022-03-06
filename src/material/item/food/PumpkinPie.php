2a5860c5b428f5fa015e4e71e7049d08eb6c048a
kotyara <62548070+kotyaralih@users.noreply.github.com>
2022-03-06 22:21:24 +0500

    Update PumpkinPie.php

--- src/material/item/food/PumpkinPie.php
+++ src/material/item/food/PumpkinPie.php
@@ -20,7 +20,7 @@
 */
 
 class PumpkinPieItem extends Item{
-	public function __construct($meta = 0){
-		parent::__construct(PUMPKIN_PIE, 0, "Pumpkin Pie");
+	public function __construct($meta = 0, $count = 1){
+		parent::__construct(PUMPKIN_PIE, 0, $count, "Pumpkin Pie");
 	}
 }
