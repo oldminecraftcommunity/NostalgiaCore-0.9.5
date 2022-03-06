08599b08670308fc15bb0f83bdd4a63be843ce44
kotyara <62548070+kotyaralih@users.noreply.github.com>
2022-03-06 22:22:17 +0500

    Update RawPorkchop.php

--- src/material/item/food/RawPorkchop.php
+++ src/material/item/food/RawPorkchop.php
@@ -20,7 +20,7 @@
 */
 
 class RawPorkchopItem extends Item{
-	public function __construct($meta = 0){
-		parent::__construct(RAW_PORKCHOP, 0, "Raw Porkchop");
+	public function __construct($meta = 0, $count = 1){
+		parent::__construct(RAW_PORKCHOP, 0, $count, "Raw Porkchop");
 	}
 }
