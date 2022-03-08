7ac6e5614b123d487b5fc277a23be874df8f027c
kotyara <62548070+kotyaralih@users.noreply.github.com>
2022-03-08 14:29:08 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,60 @@
-#!/bin/bash
+#!/usr/bin/env bash
 DIR="$(cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 cd "$DIR"
-while [ true ]
-do
-./bin/php5/bin/php -d enable_dl=On PocketMine-MP.php $@
-echo "Restarting the server..."
+
+while getopts "p:f:l" OPTION 2> /dev/null; do
+	case ${OPTION} in
+		p)
+			PHP_BINARY="$OPTARG"
+			;;
+		f)
+			POCKETMINE_FILE="$OPTARG"
+			;;
+		l)
+			DO_LOOP="yes"
+			;;
+		\?)
+			break
+			;;
+	esac
 done
+
+if [ "$PHP_BINARY" == "" ]; then
+	if [ -f ./bin/php5/bin/php ]; then
+		export PHPRC=""
+		PHP_BINARY="./bin/php5/bin/php"
+	elif [[ ! -z $(type php) ]]; then
+		PHP_BINARY=$(type -p php)
+	else
+		echo​ ​"​Couldn't find a PHP binary in system PATH or ​$PWD​/bin/php5/bin​"
+		exit 1
+	fi
+fi
+
+if [ "$POCKETMINE_FILE" == "" ]; then
+	if [ -f ./PocketMine-MP.php ]; then
+		POCKETMINE_FILE="./PocketMine-MP.php"
+	else
+		echo​ ​"​PocketMine-MP.php not found​" 
+ ​ echo​ ​"​Downloads can be found at https://github.com/kotyaralih/NostalgiaCore"
+		exit 1
+	fi
+fi
+
+LOOPS=0
+
+set +e
+
+if [ "$DO_LOOP" == "yes" ]; then
+	while true; do
+		if [ ${LOOPS} -gt 0 ]; then
+			echo "Restarted $LOOPS times"
+		fi
+		"$PHP_BINARY" "$POCKETMINE_FILE" $@
+		echo "To escape the loop, press CTRL+C now. Otherwise, wait 2 seconds for the server to restart.​"
+		sleep 2
+		((LOOPS++))
+	done
+else
+	exec "$PHP_BINARY" "$POCKETMINE_FILE" $@
+fi
