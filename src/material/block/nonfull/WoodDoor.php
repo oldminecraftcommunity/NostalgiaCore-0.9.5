513924592700ce1f862d275ee170465db77baa3b
ArkQuark <74979909+ArkQuark@users.noreply.github.com>
2022-05-04 21:13:20 +0300

    Update WoodDoor.php

--- src/material/block/nonfull/WoodDoor.php
+++ src/material/block/nonfull/WoodDoor.php
@@ -26,6 +26,17 @@ class WoodDoorBlock extends DoorBlock{
 		$this->hardness = 15;
 	}
 	
+	public function onUpdate($type){
+		if($type === BLOCK_UPDATE_NORMAL){
+			if($this->getSide(0)->getID() === AIR){ //Replace with common break method
+				ServerAPI::request()->api->entity->drop($this, BlockAPI::getItem($this->id, $this->meta, 1));
+				$this->level->setBlock($this, new AirBlock(), true, false, true);
+				return BLOCK_UPDATE_NORMAL;
+			}
+		}
+		return false;
+	}
+	
 	public function getDrops(Item $item, Player $player){
 		return array(
 			array(WOODEN_DOOR, 0, 1),
