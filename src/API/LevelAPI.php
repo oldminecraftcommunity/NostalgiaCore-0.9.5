cca35d843f832a5611b8adc984be7db176629850
Tema1d <84407490+Tema1d@users.noreply.github.com>
2022-05-31 00:02:45 +0300

    ouch, fix-remove console()
    error console()

--- src/API/LevelAPI.php
+++ src/API/LevelAPI.php
@@ -65,8 +65,7 @@ class LevelAPI{
 				$this->server->saveEnabled = true;
 				$this->saveAll();
 				$this->server->saveEnabled = $save;
-                $this->server->api->chat->broadcast("[NostalgiaCore] All data is saved.");
-            console("[NostalgiaCore] All data is saved.");
+                                $this->server->api->chat->broadcast("[NostalgiaCore] All data is saved.");
 				break;
 			case "save-on":
 				$this->server->saveEnabled = true;
