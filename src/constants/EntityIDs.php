00c56c0520c3eca44bc7ebc587e9d12aecc4b49d
ArkQuark <74979909+ArkQuark@users.noreply.github.com>
2022-06-06 23:16:09 +0300

    Entity types from GeneralConstants to EntityIDs

---
+++ src/constants/EntityIDs.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ *
+ *  ____            _        _   __  __ _                  __  __ ____  
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \ 
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/ 
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_| 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ * 
+ *
+*/
+
+//Entities
+define("ENTITY_PLAYER", 1);
+
+define("ENTITY_MOB", 2);
+	define("MOB_CHICKEN", 10);
+	define("MOB_COW", 11);
+	define("MOB_PIG", 12);
+	define("MOB_SHEEP", 13);
+
+	define("MOB_ZOMBIE", 32);
+	define("MOB_CREEPER", 33);
+	define("MOB_SKELETON", 34);
+	define("MOB_SPIDER", 35);
+	define("MOB_PIGMAN", 36);
+
+define("ENTITY_OBJECT", 3);
+	define("OBJECT_TRIPOD_CAMERA", 62);
+	define("OBJECT_PRIMEDTNT", 65);
+	define("OBJECT_ARROW", 80);
+	define("OBJECT_SNOWBALL", 81);
+	define("OBJECT_EGG", 82);
+	define("OBJECT_PAINTING", 83);
+	define("OBJECT_MINECART", 84);
+
+define("ENTITY_ITEM", 4);
+
+define("ENTITY_FALLING", 5);
+	define("FALLING_SAND", 66);
+	
+//TileEntities
+define("TILE_SIGN", "Sign");
+define("TILE_CHEST", "Chest");
+	define("CHEST_SLOTS", 27);
+define("TILE_FURNACE", "Furnace");
+	define("FURNACE_SLOTS", 3);
