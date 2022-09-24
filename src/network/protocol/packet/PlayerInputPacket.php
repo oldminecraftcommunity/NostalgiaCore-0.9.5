0c75cf8473b1e13c45fed3dd80171a4392e1bd1e
GameHerobrine <55945072+GameHerobrine@users.noreply.github.com>
2022-09-24 20:11:40 +0300

    manual adding of missing file from my commit
    *i forgot to use git add src/ =\*

---
+++ src/network/protocol/packet/PlayerInputPacket.php
@@ -0,0 +1,24 @@
+<?php
+class PlayerInputPacket extends RakNetDataPacket{
+    public $unk1, $unk2, $unk3, $unk4;
+    public function encode()
+    {
+        
+    }
+
+    public function pid()
+    {
+        return ProtocolInfo::PLAYER_INPUT_PACKET;
+    }
+
+    public function decode()
+    {
+        $this->unk1 = $this->getFloat();
+        $this->unk2 = $this->getFloat();
+        $this->unk3 = $this->getByte();
+        $this->unk4 = $this->getByte();
+        console("values: {$this->unk1}, {$this->unk2}, {$this->unk3}, {$this->unk4}");
+    }
+    
+}
+
