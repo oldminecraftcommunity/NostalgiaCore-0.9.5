1cfb43e984075f6993c74cd3d781521b55b91b6e
ArkQuark <74979909+ArkQuark@users.noreply.github.com>
2022-10-03 20:14:14 +0300

    Update Sponge.php

--- src/material/block/solid/Sponge.php
+++ src/material/block/solid/Sponge.php
@@ -2,7 +2,7 @@
 
 class SpongeBlock extends SolidBlock{
 	public function __construct(){
-		parent::__construct(SPONGE, "Sponge");
+		parent::__construct(SPONGE, 0, "Sponge");
 		$this->hardness = 3;
 	}
 	
