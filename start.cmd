233fa5a85e408d5d0312f24ac6c0e55bddef9e1f
GameHerobrine <GameHerobrine@users.noreply.github.com>
2023-01-08 12:11:27 +0300

    removed php8

--- start.cmd
+++ start.cmd
@@ -3,7 +3,7 @@ TITLE NostalgiaCore server software for Minecraft: Pocket Edition
 cd /d %~dp0
 if exist bin\php\php.exe (
 	if exist bin\mintty.exe (
-		start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o CursorType=0 -o CursorBlinks=1 -h error -t "NostalgiaCore" -i bin/pocketmine.ico -w max bin\php8test\php.exe -d enable_dl=On PocketMine-MP.php --enable-ansi %*
+		start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o CursorType=0 -o CursorBlinks=1 -h error -t "NostalgiaCore" -i bin/pocketmine.ico -w max bin\php\php.exe -d enable_dl=On PocketMine-MP.php --enable-ansi %*
 	) else (
 		bin\php\php.exe -d enable_dl=On PocketMine-MP.php %*
 	)
