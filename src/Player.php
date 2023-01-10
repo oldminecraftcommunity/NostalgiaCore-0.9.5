a97356b56a2a8c6e194945c161d53e48ccdf516c
GameHerobrine <GameHerobrine@users.noreply.github.com>
2023-01-10 18:27:27 +0300

    animatie packet fix

--- src/Player.php
+++ src/Player.php
@@ -774,10 +774,7 @@ class Player{
 				$data->lastSpeedX = $data->speedX;
 				break;
 			case "entity.animate":
-				if($data["eid"] === $this->eid or $data["entity"]->level->getName() === $this->level->getName()){
-					break;
-				}
-				console("dis");
+
 				$pk = new AnimatePacket;
 				$pk->eid = $data["eid"];
 				$pk->action = $data["action"]; //1 swing arm,
@@ -975,37 +972,13 @@ class Player{
 		/*
 		 bit mask | flag name
 		0x00000001 world_inmutable
-		0x00000002 -
-		0x00000004 -
-		0x00000008 - (autojump)
 		0x00000010 -
-		0x00000020 nametags_visible
-		0x00000040 ?
-		0x00000080 ?
-		0x00000100 ?
-		0x00000200 ?
-		0x00000400 ?
-		0x00000800 ?
-		0x00001000 ?
-		0x00002000 ?
-		0x00004000 ?
-		0x00008000 ?
-		0x00010000 ?
-		0x00020000 ?
-		0x00040000 ?
-		0x00080000 ?
-		0x00100000 ?
-		0x00200000 ?
-		0x00400000 ?
-		0x00800000 ?
+		0x00000100 -
+		0x00001000 - (autojump)
+		0x00010000 -
+		0x00100000 - (nametags_visible)
 		0x01000000 ?
-		0x02000000 ?
-		0x04000000 ?
-		0x08000000 ?
 		0x10000000 ?
-		0x20000000 ?
-		0x40000000 ?
-		0x80000000 ?
 		*/
 		$flags = 0;
 		if(($this->gamemode & 0x02) === 0x02){
