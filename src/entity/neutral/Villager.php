faf788e2d5446a8dfc4914f7ffe87b90bf3e40a5
GameHerobrine <GameHerobrine@users.noreply.github.com>
2023-02-21 23:41:28 +0300

    fixed villager

--- src/entity/neutral/Villager.php
+++ src/entity/neutral/Villager.php
@@ -5,9 +5,10 @@ class Villager extends Neutral //TODO AI
 	const TYPE = MOB_VILLAGER;
 	public function __construct(Level $level, $eid, $class, $type = 0, $data = []){
 		$this->setSize(0.6, 1.8);
-		$this->setSpeed(0.3);
-		$this->setName("Villager");
 		parent::__construct($level, $eid, $class, $type, $data);
+		$this->setSpeed(0.3);
+		$this->setHealth(isset($this->data["Health"]) ? $this->data["Health"] : 10, "generic");
+		$this->setName("Villager");
 	}
 }
 
