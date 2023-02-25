cacfa42413bd66b459582c1846728c7e7d676c16
GameHerobrine <GameHerobrine@users.noreply.github.com>
2023-02-25 12:05:18 +0300

    added missing 0.9.5 biomes to id list

--- src/constants/BiomeIDs.php
+++ src/constants/BiomeIDs.php
@@ -4,40 +4,47 @@
 define("OCEAN", 0);
 define("PLAINS", 1);
 define("DESERT", 2);
-define("MOUNTAINS", 3); //extreme_hills
+define("EXTREME_HILLS", 3); //extreme_hills
 define("FOREST", 4);
 define("TAIGA", 5);
 define("SWAMP", 6);
 define("RIVER", 7);
-
-define("LEGACY_FROZEN_OCEAN", 10);
+define("HELL", 8); //yes it is a biome in 0.9
+//id 9 found, not sure about name, maybe sky
+define("FROZEN_OCEAN", 10);
 define("FROZEN_RIVER", 11);
 define("ICE_PLAINS", 12);
-
+define("ICE_MOUNTAINS", 13); //snow covered, temp&fall->0.0, 0.5
 define("MUSHROOM_ISLAND", 14);
 define("MUSHROOM_ISLAND_SHORE", 15);
 define("BEACH", 16);
 define("DESERT_HILLS", 17);
-
-define("SMALL_MOUNTAINS", 20);
+define("FOREST_HILLS", 18);
+define("TAIGA_HILLS", 19);
+define("EXTREME_HILLS_EDGE", 20);
 define("JUNGLE", 21);
 define("JUNGLE_HILLS", 22);
-define("JUNGLE_ENGE", 23);
+define("JUNGLE_EDGE", 23);
 define("DEEP_OCEAN", 24);
-
+define("STONE_BEACH", 25);
+define("COLD_BEACH", 26);
 define("BIRCH_FOREST", 27);
-    
+define("BIRCH_FOREST_HILLS", 28);
 define("ROOFED_FOREST", 29);
-
+define("COLD_TAIGA", 30);
+define("COLD_TAIGA_HILLS", 31);
+define("MEGA_TAIGA", 32);
+define("MEGA_TAIGA_HILLS", 33);
+define("EXTREME_HILLS_PLUS", 34);
 define("SAVANNA", 35);
 define("SAVANNA_PLATEAU", 36);
 define("MESA", 37);
-define("MESA_PLATEAU_STONE", 38);
+define("MESA_PLATEAU_F", 38);
+define("MESA_PLATEAU", 39);
+//define("SUNFLOWER_PLAINS", 129); metioning not found
 
-define("SUNFLOWER_PLAINS", 129);
+//define("ICE_PLAINS_SPIKES", 140); metioning not found
 
-define("ICE_PLAINS_SPIKES", 140);
-
-define("REDWOOD_TAIGA_MUTATED", 160);
+define("REDWOOD_TAIGA_MUTATED", 160); //mentioned, but cannot check id
 
 define("MAX_BIOMES", 256);
