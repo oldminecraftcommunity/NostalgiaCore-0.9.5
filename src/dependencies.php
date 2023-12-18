48cf686a2f17ccb1efd1aa1e291071adec915ddf
GameHerobrine <waterfall009@gmail.com>
2023-12-18 12:03:39 +0300

    when autoload

--- src/dependencies.php
+++ src/dependencies.php
@@ -76,6 +76,7 @@ if($errors > 0){
 $sha1sum = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
 /***REM_START***/
 require_once(FILE_PATH . "/src/math/Vector3.php");
+require_once(FILE_PATH . "/src/math/AxisAlignedBB.php");
 require_once(FILE_PATH . "/src/world/Position.php");
 require_once(FILE_PATH . "/src/pmf/PMF.php");
 
