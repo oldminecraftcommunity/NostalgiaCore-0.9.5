f861e898d467a8508de874ba4716b3bad142f264
GameHerobrine <waterfall009@gmail.com>
2024-09-28 09:56:58 +0300

    fixed teleport

--- src/Player.php
+++ src/Player.php
@@ -219,9 +219,12 @@ class Player{
 						$player->sendArmor($this);
 					}
 				}
+				$resyncpos = new Position($pos->x, $pos->y, $pos->z, $pos->level);
+			}else{
+				$resyncpos = new Vector3($pos->x, $pos->y, $pos->z);
 			}
 
-			$this->lastCorrect = $pos;
+			$this->lastCorrect = $resyncpos;
 			$this->entity->fallY = false;
 			$this->entity->fallStart = false;
 			//if($terrain === true){
@@ -230,7 +233,7 @@ class Player{
 			//}*/
 			$this->entity->check = true;
 			if($force === true){
-				$this->forceMovement = $pos;
+				$this->forceMovement = $resyncpos;
 			}
 		}
 		if($this->spawned){
